d4c68cb02ddc547e0d220a31c26cc46bc398baac
Mario <mariobecker2006@outlook.com>
2026-02-19 17:25:18 +0100

    initial version

---
+++ src/download_cleaner.bat
@@ -0,0 +1,3 @@
+@echo off
+py "C:\Users\mario\OneDrive\VSC\python\explorer_cleaner\src\main.py"
+pause
---
+++ src/main.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+
+def main(p: str) -> None:
+    if next(os.scandir(p), None) is None:
+        print("Folder is empty")
+    else:
+        print(f"Files in this folder:\n")
+        for i in os.listdir(p):
+            print(f"{i}\n")
+        user_input = input("Folder isnt empty, are you sure u want delete everything?\n")
+        if user_input in ["y", "1", "yes"]:
+            for i in os.listdir(p):
+                full = os.path.join(p, i)
+                if os.path.isfile(full):
+                    os.remove(full)
+                else:
+                    shutil.rmtree(full)
+        else:
+            print("Folder will not be cleaned, end of task")
+
+if __name__ == "__main__":
+    main("C:\\Users\\mario\\Downloads")
